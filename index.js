b305defd6daea57fd118f50a337cadae7fb17c4a
Daniel <holzigitovdaniel73@mail.com>
2025-09-02 12:43:34 +0600

    salam

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let result = document.querySelector(".result")
 
 result.innerHTML ="Hello World"
 
-let text = document.querySelector("text")
-text.innerHTML ="Daniel"
+let salam = document.querySelector("salam")
+salam.innerHTML ="Daniel"
